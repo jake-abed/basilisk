d63588b3a7633145a9a5ec3a41902e158887a1d1
Jake Abed <jakeabed@gmail.com>
2024-06-30 13:31:19 -0700

    Add test login page and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-center p-24'>
-			<div className='flex justify-center items-center min-w-72 min-h-[50svh] bg-stone-900 rounded-xl p-8'>
-				<h1 className='text-center text-3xl text-stone-200 font-bold'>
+			<div className='flex justify-center items-center flex-col min-w-72 min-h-[50svh] bg-stone-900 rounded-xl p-8 gap-8'>
+				<h1 className='text-center text-3xl text-stone-200 font-extrabold'>
 					WELCOME TO BASILISK
 				</h1>
+				<Link
+					href='/login'
+					className='text-xl text-center w-1/2 font-bold text-stone-200 bg-stone-600 px-12 py-4 rounded-xl hover:bg-stone-700'
+				>
+					LOGIN
+				</Link>
+				<Link
+					href='/signup'
+					className='text-xl text-center w-1/2 font-bold text-stone-200 bg-stone-600 px-12 py-4 rounded-xl hover:bg-stone-700'
+				>
+					SIGNUP
+				</Link>
 			</div>
 		</main>
 	);
