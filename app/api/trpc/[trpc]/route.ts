2f71515ba290a15c1dc678f196b57119cfecaf89
Jake Abed <jakeabed@gmail.com>
2024-07-20 14:06:24 -0700

    Add temporary ts-ignore to context arg in trpc handler

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,13 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/server/index';
+import { type Context } from '@/server/context';
 
 const handler = (req: Request) =>
 	fetchRequestHandler({
 		endpoint: '/api/trpc',
 		req,
 		router: appRouter,
-		createContext: () => ({}),
+		//@ts-ignore
+		createContext: (ctx: Context) => ctx,
 	});
-
 export { handler as GET, handler as POST };
