211c203e4a7763134802ee741bd54a5aed116a62
Jake Abed <jakeabed@gmail.com>
2024-07-24 16:39:05 -0700

    Add basic note view to home page and test it out

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,19 @@
 'use client';
+import { trpc } from '../_trpc/client';
 
 export default function Page() {
+	const mostRecentNotes = trpc.notes.getMostRecentNotes.useQuery({
+		limit: 10,
+		page: 1,
+	});
+
 	return (
-		<>
-			<p>STUFF</p>
-		</>
+		<div className='flex flex-col gap-4 p-4'>
+			{mostRecentNotes?.data
+				? mostRecentNotes.data.map((note) => (
+						<p className='bg-emerald-950 p-2'>{note.content}</p>
+				  ))
+				: null}
+		</div>
 	);
 }
